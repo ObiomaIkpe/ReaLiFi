9bf72a5652c0287c0e9084440e057e29796d33ce
ObiomaIkpe <obiomaikpe1@gmail.com>
2025-10-31 08:20:30 +0100

    made changes to header and favicon, removed some comments

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,8 @@ import './index.css';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { darkTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { wagmiConfig } from './wagmi.config.js'; // Your existing config file
+import { wagmiConfig } from './wagmi.config.js';
 
-// Import RainbowKit styles
 import '@rainbow-me/rainbowkit/styles.css';
 
 const queryClient = new QueryClient();
@@ -18,11 +17,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider theme={darkTheme({
-  accentColor: '#CAAB5B', // Your gold color
-  accentColorForeground: '#121317', // Your main color
-  borderRadius: 'large',
-})}>
-      
+                accentColor: '#CAAB5B',
+                accentColorForeground: '#121317',
+                borderRadius: 'large',
+})}>      
           <App />
         </RainbowKitProvider>
       </QueryClientProvider>
